c366109317180d5568e0dab14f9a5fdad6e19fe2
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 19:09:20 -0400

    Summaries for all methods in user controller

--- Spot.Backend/Controllers/UsersController.cs
+++ Spot.Backend/Controllers/UsersController.cs
@@ -20,6 +20,9 @@ namespace OmegaSpot.Backend.Controllers {
         //This region contains everything to do with Logging in, Logging out, and changing passwords
         #region Auth
 
+        /// <summary>Attempts to sign in a user by checking if the user exists, and if their password matches with the one given</summary>
+        /// <param name="U"></param>
+        /// <returns><see cref="Guid.Empty"/> if the username or password were not found, or a new guid if a session was registered</returns>
         // POST: Auth
         [HttpPost("Auth")]
         public async Task<IActionResult> LogIn(UserAuthRequest U) {
@@ -38,12 +41,18 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(Guid.Empty);
         }
 
+        /// <summary>Attempts to log out a user, by removing the given session from the session manager</summary>
+        /// <param name="SessionID"></param>
+        /// <returns>true if logout was successful, false if the session was not found</returns>
         //POST: Auth/Out
         [HttpPost("Auth/Out")]
         public async Task<IActionResult> LogOut(Guid SessionID) {
             return Ok(SessionManager.Manager.LogOut(SessionID));
         }
 
+        /// <summary>Attempts to log out of all sessions that have the same user tied to it as the given session does</summary>
+        /// <param name="SessionID"></param>
+        /// <returns>number of sessions signed out of, or unauthorized if the session was not found</returns>
         //POST: Auth/Out
         [HttpPost("Auth/OutAll")]
         public async Task<IActionResult> LogOutAll(Guid SessionID) {
@@ -53,6 +62,9 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(SessionManager.Manager.LogOutAll(S.UserID));
         }
 
+        /// <summary>Attempts to change the password of a user with the session ID in the given password change request</summary>
+        /// <param name="PCR"></param>
+        /// <returns>True if the password change was successful. Bad request if it does not, with a given reason.</returns>
         // PUT: Auth
         [HttpPut("Auth")]
         public async Task<IActionResult> ChangePassword(PasswordChangeRequest PCR) {
@@ -81,6 +93,9 @@ namespace OmegaSpot.Backend.Controllers {
 
         //Hey maybe if someone could find a better anme for this I'd appreciate it
 
+        /// <summary>Attempts to change the name of a user with the session ID in the given name change request</summary>
+        /// <param name="NCR"></param>
+        /// <returns>True if the name change was successful. Bad request if it does not, with a given reason.</returns>
         // POST: ChangeName
         [HttpPost("ChangeName")]
         public async Task<IActionResult> ChangeName(NameChangeRequest NCR) {
@@ -108,6 +123,9 @@ namespace OmegaSpot.Backend.Controllers {
         //This region handles management of notifications
         #region Manage Notifications
 
+        /// <summary>Gets all notifs from the user tied to the given session ID</summary>
+        /// <param name="SessionID"></param>
+        /// <returns>List of all notifs from the given user, or unauthorized if the given session was not found</returns>
         [HttpPost("Notif")]
         public async Task<IActionResult> GetNotifs(Guid SessionID) {
             Session S = SessionManager.Manager.FindSession(SessionID);
@@ -121,6 +139,10 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(Ns);
         }
 
+        /// <summary>Marks a given notif as read if it belongs to the session ID</summary>
+        /// <param name="NotifID"></param>
+        /// <param name="SessionID"></param>
+        /// <returns>The given notification, or unauthorized if the session was not found, or if the notif does not belong to the sessions's tied user</returns>
         //POST Notif/5
         [HttpPost("Notif/{NotifID}")]
         public async Task<IActionResult> ReadNotif(Guid NotifID, [FromBody] Guid SessionID) {
@@ -141,6 +163,9 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(N);
         }
 
+        /// <summary>Marks all unread notifications from the session's tied user as read</summary>
+        /// <param name="SessionID"></param>
+        /// <returns>Number of notifications marked as read, or unauthorized if the session does not exist</returns>
         //POST Notif/All
         [HttpPost("Notif/all")]
         public async Task<IActionResult> ReadNotifAll(Guid SessionID) {
@@ -164,6 +189,9 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(UpdatedNotifs);
         }
 
+        /// <summary>Delets all notifications that are read from a session's tied user.</summary>
+        /// <param name="SessionID"></param>
+        /// <returns>Number of notifications deleted</returns>
         //DELTE Notif
         [HttpDelete("Notif")]
         public async Task<IActionResult> ReadNotifDel(Guid SessionID) {
@@ -186,6 +214,9 @@ namespace OmegaSpot.Backend.Controllers {
         //This region handles the getting of user details
         #region User Details
 
+        /// <summary>Gets details of the user tied to the given session id</summary>
+        /// <param name="SessionID"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> UserDetails(Guid SessionID) {
 
@@ -198,6 +229,9 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(DBU);
         }
 
+        /// <summary>Gets reservations of the user tied to the given session id</summary>
+        /// <param name="SessionID"></param>
+        /// <returns></returns>
         [HttpPost("Reservations")]
         public async Task<IActionResult> UserReservations(Guid SessionID) {
 
@@ -213,6 +247,9 @@ namespace OmegaSpot.Backend.Controllers {
             return Ok(DBU);
         }
 
+        /// <summary>Gets business of the user tied to the given session id if said user is a business owner</summary>
+        /// <param name="SessionID"></param>
+        /// <returns></returns>
         [HttpPost("Business")]
         public async Task<IActionResult> UserBusiness(Guid SessionID) {
             Session S = SessionManager.Manager.FindSession(SessionID);
