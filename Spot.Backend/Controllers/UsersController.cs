fc9aff9da2fbcfc746eea2a987e714b93be26eec
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 19:24:19 -0400

    Load All notifs, not just those that are not read

--- Spot.Backend/Controllers/UsersController.cs
+++ Spot.Backend/Controllers/UsersController.cs
@@ -133,7 +133,7 @@ namespace OmegaSpot.Backend.Controllers {
 
             //Load Notif
             List<Notification> Ns = await _context.Notification
-                .Where(N => N.User.Username == S.UserID && !N.Read)
+                .Where(N => N.User.Username == S.UserID)
                 .OrderByDescending(N => N.Read).ThenByDescending(N => N.SentTime)
                 .ToListAsync();
             if (Ns == null) { return NotFound(); }
