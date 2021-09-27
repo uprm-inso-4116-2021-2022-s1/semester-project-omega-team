bd1f37513a58214ce61583815a51a3341903c82a
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 18:20:36 -0400

    Add Password Change Request

---
+++ Spot.Backend/Requests/PasswordChangeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace OmegaSpot.Backend.Requests {
+    public class PasswordChangeRequest:ISpotRequest {
+        /// <summary>ID of the session this PCR comes from</summary>
+        public System.Guid SessionID { get; set; }
+
+        /// <summary>User's current password</summary>
+        public string CurrentPassword { get; set; }
+
+        /// <summary>New Password</summary>
+        public string NewPassword { get; set; }
+    }
+}
