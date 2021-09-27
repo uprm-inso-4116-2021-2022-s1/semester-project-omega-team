35403405c52f01d8c318ab4b31170c87c3df6136
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 18:11:39 -0400

    Create User Auth Request for login

---
+++ Spot.Backend/Requests/UserAuthRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spot.Backend.Requests {
+
+    /// <summary>Request to authenticate and log in a user</summary>
+    public class UserAuthRequest : ISpotRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public Guid SessionID { get; set; }
+
+        /// <summary>Username of the user</summary>
+        public string Username { get; set; }
+
+        /// <summary>Password of the User</summary>
+        public string Password { get; set; }
+
+    }
+}
