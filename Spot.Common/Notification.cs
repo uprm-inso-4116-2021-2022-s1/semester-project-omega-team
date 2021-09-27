7f6105d5d2584fb679a577511ccf36ed1fa06bb9
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 13:24:13 -0400

    Add Notification

---
+++ Spot.Common/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Spot.Common {
+    /// <summary>Holds a Spot notification, which informs a <see cref="Common.User"/> about <see cref="Reservation"/> changes</summary>
+    public class Notification: ISpotBase {
+
+        /// <summary>ID of this notification</summary>
+        public Guid ID { get; set; }
+
+        /// <summary>Time and date at which it was sent out</summary>
+        public DateTime SentTime { get; set; }
+
+        /// <summary>User this notification was sent to</summary>
+        public User User { get; set; }
+
+        /// <summary>Text of this notification</summary>
+        public string Text { get; set; }
+
+        /// <summary>Whether or not this notification has been read by the user</summary>
+        public bool Read { get; set; }
+    }
+}
