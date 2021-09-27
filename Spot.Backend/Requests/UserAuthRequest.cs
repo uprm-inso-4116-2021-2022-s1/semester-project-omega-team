a8e5b8308583863d86c60bc055623a4886350f2b
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 18:15:06 -0400

    UserAuth is no longer ISpotRequest because it can't have a Session ID (it is what grants a user a session ID)

--- Spot.Backend/Requests/UserAuthRequest.cs
+++ Spot.Backend/Requests/UserAuthRequest.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 namespace OmegaSpot.Backend.Requests {
 
     /// <summary>Request to authenticate and log in a user</summary>
-    public class UserAuthRequest : ISpotRequest {
-
-        /// <summary>ID of the session executing this request</summary>
-        public Guid SessionID { get; set; }
+    public class UserAuthRequest {
 
         /// <summary>Username of the user</summary>
         public string Username { get; set; }
