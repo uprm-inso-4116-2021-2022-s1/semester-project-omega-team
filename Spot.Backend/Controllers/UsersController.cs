ce78c22001cc482da6040c5bfd9a8e2df972546b
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 14:08:19 -0400

    Add empty Users controller

---
+++ Spot.Backend/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Spot.Common;
+using Spot.Data;
+
+namespace Spot.Backend.Controllers {
+
+    [Route("User")] //This is the route the URL needs to go to
+    [ApiController] //This indicates there's an API Controller 
+    public class UsersController: Controller {
+        private readonly SpotContext _context;
+
+        public UsersController(SpotContext context) { _context = context; }
+    
+    
+    }
+}
