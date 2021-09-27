fbd4ec6807c9118694382ca5b5ee2ac3397f1f23
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 14:07:53 -0400

    add empty Reservations and Spots controller

---
+++ Spot.Backend/Controllers/ReservationsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Spot.Common;
+using Spot.Data;
+
+namespace Spot.Backend.Controllers {
+
+    [Route("Reservation")]
+    [ApiController]
+    public class ReservationsController : Controller {
+        private readonly SpotContext _context;
+
+        public ReservationsController(SpotContext context) { _context = context; }
+
+    }
+}
---
+++ Spot.Backend/Controllers/SpotsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Spot.Common;
+using Spot.Data;
+
+namespace Spot.Backend.Controllers {
+    
+    [Route("Spot")]
+    [ApiController]
+    public class SpotsController : Controller {
+        private readonly SpotContext _context;
+
+        public SpotsController(SpotContext context) { _context = context; }
+    }
+}
