72845b5ddccc6dce4211928e6c4be808f70d97c3
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 18:10:08 -0400

    Create ISpotRequest which holds interface for all future request objects

---
+++ Spot.Backend/Requests/ISpotRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spot.Backend.Requests {
+
+    /// <summary>Minimum common information for a Spot request.</summary>
+    public interface ISpotRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public Guid SessionID { get; set; }
+
+    }
+}
