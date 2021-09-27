9d085445dd885e2ce23ae35b9137b2fdc06b2a86
Ignacio Tampe <Igtampe@gmail.com>
2021-09-27 18:26:04 -0400

    Add Auth portion of User Controller

--- Spot.Backend/Controllers/UsersController.cs
+++ Spot.Backend/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OmegaSpot.Common;
 using OmegaSpot.Data;
+using OmegaSpot.Backend.Requests;
 
 namespace OmegaSpot.Backend.Controllers {
 
@@ -16,7 +17,66 @@ namespace OmegaSpot.Backend.Controllers {
         private readonly SpotContext _context;
 
         public UsersController(SpotContext context) { _context = context; }
-    
-    
+
+        //This region contains everything to do with Logging in, Logging out, and changing passwords
+        #region Auth
+
+        // POST: Auth
+        [HttpPost("Auth")]
+        public async Task<IActionResult> Check(UserAuthRequest U) {
+            if (string.IsNullOrEmpty(U.Username)) { return BadRequest("Blank User Auth object"); }
+
+            //Find a user:
+            User DBU = await _context.User.FindAsync(U.Username);
+            if (DBU == null) { return Ok(Guid.Empty); }
+
+            if (DBU.Equals(U)) {
+                //Log in
+                return Ok(SessionManager.Manager.LogIn(U.Username));
+            }
+
+            //Otherwise return an empty guid
+            return Ok(Guid.Empty);
+        }
+
+        //POST: Auth/Out
+        [HttpPost("Auth/Out")]
+        public async Task<IActionResult> LogOut(Guid SessionID) {
+            return Ok(SessionManager.Manager.LogOut(SessionID));
+        }
+
+        //POST: Auth/Out
+        [HttpPost("Auth/OutAll")]
+        public async Task<IActionResult> LogOutAll(Guid SessionID) {
+            Session S = SessionManager.Manager.FindSession(SessionID);
+            if (S == null) { return Unauthorized("Invalid session"); }
+
+            return Ok(SessionManager.Manager.LogOutAll(S.UserID));
+        }
+
+        // PUT: Auth
+        [HttpPut("Auth")]
+        public async Task<IActionResult> Update(PasswordChangeRequest PCR) {
+            if (PCR.SessionID == Guid.Empty) { return BadRequest("Blank Session ID"); }
+
+            //Find Session
+            Session S = SessionManager.Manager.FindSession(PCR.SessionID);
+            if (S == null) { return BadRequest("Session ID not found"); }
+
+            //Find a user:
+            User DBU = await _context.User.FindAsync(S.UserID);
+            if (DBU.Password != PCR.CurrentPassword) { return BadRequest("Password did not match"); }
+
+            //OK go for change
+            DBU.Password = PCR.NewPassword;
+            _context.Update(DBU);
+            await _context.SaveChangesAsync();
+
+            return Ok(true);
+        }
+
+        #endregion
+
+
     }
 }
