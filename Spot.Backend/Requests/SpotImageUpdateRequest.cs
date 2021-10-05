b31f9472df27109011552f5da281f7bf7490c5cb
Ignacio Tampe <Igtampe@gmail.com>
2021-10-05 13:51:34 -0400

    Add requests needed for CRUD

---
+++ Spot.Backend/Requests/SpotImageUpdateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OmegaSpot.Common;
+
+namespace OmegaSpot.Backend.Requests {
+
+    /// <summary>Holds a request to update a specified plot's image</summary>
+    public class SpotImageUpdateRequest:ISpotRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public Guid SessionID { get; set; }
+
+        /// <summary>ID of the plot to update the image of</summary>
+        public Guid PlotID { get; set; }
+
+        /// <summary>Spot's new image</summary>
+        public byte[] Image { get; set; }
+
+    }
+}
