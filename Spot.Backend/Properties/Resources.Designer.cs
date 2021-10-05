13b2ab0b33c8865d8ef373deae1d806d4eb97804
Ignacio Tampe <Igtampe@gmail.com>
2021-10-05 14:51:49 -0400

    Add DefualtSpot image to Resources

--- Spot.Backend/Properties/Resources.Designer.cs
+++ Spot.Backend/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace OmegaSpot.Backend.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Spot.Backend.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OmegaSpot.Backend.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -57,6 +57,16 @@ namespace OmegaSpot.Backend.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DefaultSpot {
+            get {
+                object obj = ResourceManager.GetObject("DefaultSpot", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
