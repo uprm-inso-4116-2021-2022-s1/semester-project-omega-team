230fa9e0f4b135c46e7c23701201f3faf14736c2
Francis Maclang <josefrancisco.maclang@upr.edu>
2021-10-06 18:40:56 -0400

    pulled changes from staging

--- Spot.Frontend/ClientApp/src/App.js
+++ Spot.Frontend/ClientApp/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import LogIn from './components/UserAuth/LogIn/LogIn';
+import SignUp from './components/UserAuth/SignUp/SignUp';
+import MainPage from './components/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LogIn />
+      <SignUp />
+      <MainPage />
     </div>
   );
 }
---
+++ Spot.Frontend/ClientApp/src/components/MainPage/MainPage.js
@@ -0,0 +1,7 @@
+export default function MainPage() {
+    return(
+        <div>
+            <p>testing, MainPage works!</p>
+        </div>
+    )
+}
