39cbd07e7d474694a0826742c1e389a6f1da7f22
Francis Maclang <josefrancisco.maclang@upr.edu>
2021-10-06 21:28:55 -0400

    generated file structure, added app bar, added routing

--- Spot.Frontend/ClientApp/src/App.js
+++ Spot.Frontend/ClientApp/src/App.js
@@ -1,15 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 import LogIn from './components/UserAuth/LogIn/LogIn';
 import SignUp from './components/UserAuth/SignUp/SignUp';
 import MainPage from './components/MainPage/MainPage';
+import BusinessPage from './components/MainPage/Business/BusinessPage';
+import ClientPage from './components/MainPage/Client/ClientPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LogIn />
-      <SignUp />
-      <MainPage />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/mainpage">
+            <MainPage />
+          </Route>
+          <Route path="/businesspage">
+            <BusinessPage />
+          </Route>
+          <Route path="/clientpage">
+            <ClientPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- Spot.Frontend/ClientApp/src/components/MainPage/MainPage.js
+++ Spot.Frontend/ClientApp/src/components/MainPage/MainPage.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
 export default function MainPage() {
-    return(
+
+    const [accountType, setAccountType] = useState("");
+
+    const checkAccount = () => {
+        console.log("checking account type now:", accountType);
+        if (accountType === "business") {
+            return (<Redirect to="/businesspage" />);
+        } else {
+            return (<Redirect to="/clientpage" />);
+        }
+    }
+
+    return (
         <div>
             <p>testing, MainPage works!</p>
+            {checkAccount()}
         </div>
     )
 }
