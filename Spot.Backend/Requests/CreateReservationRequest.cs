d01e58b5963b3b9b61bc68be6f021d753ab7286a
Ignacio Tampe <Igtampe@gmail.com>
2021-10-28 14:09:25 -0400

    Create and update reservation requests

---
+++ Spot.Backend/Requests/CreateReservationRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OmegaSpot.Backend.Requests {
+    
+    /// <summary>Request to create a reservation</summary>
+    public class CreateReservationRequest:ISpotRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public Guid SessionID { get; set; }
+
+        /// <summary>Reason this reservation is being made</summary>
+        public string Reason { get; set; }
+
+        /// <summary>Time this reservation will start at</summary>
+        public DateTime StartTime { get; set; }
+
+        /// <summary>Time this reservation will end at</summary>
+        public DateTime EndTime { get; set; }
+
+        /// <summary>ID Of the spot to Reserve</summary>
+        public Guid SpotID { get; set; }
+
+    }
+}
---
+++ Spot.Backend/Requests/UpdateReservationRequest.cs
@@ -0,0 +1,18 @@
+﻿using OmegaSpot.Common;
+
+namespace OmegaSpot.Backend.Requests {
+
+    /// <summary>Request to update a reservation's status</summary>
+    public class UpdateReservationRequest:ISpotRequest {
+
+        /// <summary>ID of the session executing this request</summary>
+        public System.Guid SessionID { get; set; }
+
+        /// <summary>ID of the reservation to update the status of</summary>
+        public System.Guid ReservationID { get; set; }
+
+        /// <summary>New status of the reservation</summary>
+        public ReservationStatus NewStatus { get; set; }
+
+    }
+}
